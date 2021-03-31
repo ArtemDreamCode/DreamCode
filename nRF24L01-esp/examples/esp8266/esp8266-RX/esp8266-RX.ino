be08c04a5c3fdcf7f2cf309ea782f8d150be2ebc
ArtemShest <artem.shest@yandex.ru>
2021-03-31 23:27:33 +0300

    678

---
+++ nRF24L01-esp/examples/esp8266/esp8266-RX/esp8266-RX.ino
@@ -0,0 +1,70 @@
+diff --git a/nRF24L01-esp/examples/esp8266/esp8266-RX/esp8266-RX.ino b/nRF24L01-esp/examples/esp8266/esp8266-RX/esp8266-RX.ino
+deleted file mode 100644
+index 50215ca..0000000
+--- a/nRF24L01-esp/examples/esp8266/esp8266-RX/esp8266-RX.ino
++++ /dev/null
+@@ -1,64 +0,0 @@
+-//приемник
+-//nRF24L01 - ESP8266 12
+-//1 GRND
+-//2 VCC
+-//3 CE   - GPIO4   5 d2  
+-//4 CSN  - GPIO15  d8
+-//5 SCK  - GPIO14  d5
+-//6 MOSI - GPIO13  d7
+-//7 MISO - GPIO12  d6
+-//8 IRQ  - NC
+- 
+-#include <SPI.h>
+-#include "nRF24L01.h"
+-#include "RF24.h"
+-RF24 radio(4, 15); 
+-
+-byte address[][6] = {"1Node","2Node","3Node","4Node","5Node","6Node"};  //возможные номера труб
+-
+-int lamp_pin = 0; //d3 пин управления реле
+-int ti=0;
+-
+-void setup(){     
+-  SPI.setHwCs(true);
+-  SPI.begin();
+-  SPI.setDataMode(SPI_MODE0);
+-  SPI.setBitOrder(MSBFIRST);   
+-    Serial.begin(9600);  
+-    radio.begin();
+-    radio.setAutoAck(0);
+-    radio.setRetries(0,15);
+-    radio.enableAckPayload();
+-    radio.setPayloadSize(32);
+-    radio.setChannel(0x60);
+-    radio.setPALevel      (RF24_PA_MAX);
+-    radio.setDataRate     (RF24_1MBPS);
+-    
+-    radio.powerUp(); //начать работу
+-    delay(10);
+-    radio.startListening();
+-    delay(10);
+-    radio.openReadingPipe (1, address[0]);
+-//  radio.stopListening   (); 
+-    Serial.println("Radio configured!");
+-    delay(100);
+-    pinMode(lamp_pin, OUTPUT);
+-    digitalWrite(lamp_pin, LOW);
+-}
+-
+-void loop(){
+-  byte pipeNo, gotByte;     
+-  while(radio.available()){
+-    radio.read(&gotByte, sizeof(gotByte));
+-    Serial.print("Recieved: ");
+-    Serial.println(gotByte);
+-    if (gotByte==100)
+-    {
+-      digitalWrite(lamp_pin, HIGH);
+-    }
+-    else if (gotByte==200)
+-    {
+-      digitalWrite(lamp_pin, LOW);
+-    }
+-  }
+-}
