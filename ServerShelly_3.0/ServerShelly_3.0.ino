7679d128378ca374aa0e90f36a0558faf22af619
Dorofeev <dorofeev@kinoplan.ru>
2021-09-09 21:17:28 +0300

    fix

--- ServerShelly_3.0/ServerShelly_3.0.ino
+++ ServerShelly_3.0/ServerShelly_3.0.ino
@@ -5,30 +5,26 @@
 #include <EEPROM.h>
 #include "UnitFlash.h"
 
+#define EEPROM_NameAddr 0
+#define EEPROM_StateAddr 1
 
 ESP8266WebServer server(80);
 
 WiFiClient client;  
-String Device_GUID = "dDf5FFShellysde";
-int Device_Position = 0;
+
 bool RealCheck = false;
-int bt_state;
+int bt_state = 0;
 String MacAdr;
 String ClassDevice = "Shelly";
 String DeviceFrendlyName = "New Robotic Device";
-int DeviceIndex = 0;
+String Device_GUID = "dDf5FFShellysde";
+
 //const char* ssid = "R_302";
 //const char* pass = "ProtProtom";
-//const char* ssid = "rostelecom_104";
-//const char* pass = "63030723";
 
 
-//const char* ssid = "TP-LINK_120";
-//const char* pass = "160193ya";
-
-
-const char* ssid = "Keenetic-9462";
-const char* pass = "LCMN8S6X";
+const char* ssid = "TP-LINK_120";
+const char* pass = "160193ya";
 
 
 //const char* ssid = "iPhonexc5";
@@ -40,42 +36,13 @@ const char* pass = "LCMN8S6X";
 //const char* ssid = "ESPap";
 //const char* pass = "123456789";
 
-void DoCheckButtonState(){
-  int bt = digitalRead(5);
-  if (bt != bt_state)
-  { //если есть изменение состояния кнопки
-     if (!RealCheck)
-     {
-       RealCheck = true;
-       digitalWrite(4, HIGH); //включаем
-     }
-     else if (RealCheck)
-     {
-       RealCheck = false;
-       digitalWrite(4, LOW); //выключаем
-     }
-     eeprom_write_state(RealCheck);
-  }
-  else
-  {
-    if (RealCheck) //realcheck уже начитался из памяти 
-       digitalWrite(4, HIGH); //включаем
-    else      
-       digitalWrite(4, LOW); //выключаем
-  }
- 
-  bt_state = bt; //готовы снова ловить выключатель  
-}
-
 void wifi_begin(){
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid); 
- // WiFi.begin(ssid, pass);  
+  WiFi.begin(ssid, pass);  
   while (WiFi.status() != WL_CONNECTED) {
     digitalWrite(2, HIGH);  
     Serial.println("connecting to wifi");
     delay(450);
-    DoCheckButtonState();
   }
   Serial.println(WiFi.localIP());
   digitalWrite(2, LOW); 
@@ -105,7 +72,6 @@ void restServerRouting() {
             F("Welcome to the REST Shelly Server"));
     });
     server.on(("/relay"), HTTP_GET, handle_ChangeState); // get from user
-    server.on(("/reset"), HTTP_GET, handle_Reset); // get from user
     server.on(("/state"), HTTP_GET, handle_GetState); // get from AP
     server.on(("/set"), HTTP_GET, handle_ChangeFrendlyName); // change device name
 }
@@ -118,22 +84,14 @@ void setup()
   pinMode(4, OUTPUT);
   digitalWrite(4, LOW);
   pinMode(5, INPUT);
-    
-  DeviceFrendlyName = eeprom_read_name();
-  RealCheck = eeprom_read_state();
-  DeviceIndex = eeprom_read_index();
-  bt_state = digitalRead(5);
+  EEPROM.begin(16);
+//  DeviceFrendlyName = readEEPROM();
+  //RealCheck = GetFlashState();
   Serial.println(RealCheck);
   wifi_begin();
   restServerRouting();
-  
   server.onNotFound(handleNotFound);
   server.begin();
-
-  if(!is_new_device())
-  {
-      handle_Reset();
-  }
 }
 
 void handle_GetState(){ //запрос о состоянии от клиента
@@ -147,17 +105,10 @@ void handle_GetState(){ //запрос о состоянии от клиента
      st = "off";
    }
    response+= "\"device_guid\": \""+Device_GUID+"\"";
-   response+= ",\"index\": \""+String(DeviceIndex)+"\""; 
    response+= ",\"state\": \""+st+"\"";
    response+= ",\"ip\": \""+WiFi.localIP().toString()+"\"";
    response+= ",\"class\": \""+ClassDevice+"\""; 
    response+= ",\"name\": \""+DeviceFrendlyName+"\""; 
-   char newdev = eeprom_read_state_new_device();
-   Serial.println(newdev);
-   if (newdev == 1)
-      response+= ",\"isnewdevice\": \"old\"";
-   else 
-      response+= ",\"isnewdevice\": \"new\"";
    response+= ",\"mac\": \""+MacAdr+"\"";   
    response+= ",\"gw\": \""+WiFi.gatewayIP().toString()+"\"";
    response+= ",\"nm\": \""+WiFi.subnetMask().toString()+"\"";
@@ -170,6 +121,23 @@ void handle_GetState(){ //запрос о состоянии от клиента
    response+="}";
   
    server.send(200, "text/html", response);
+
+/*   
+    if (server.arg("signalStrength")== "true"){
+        response+= ",\"signalStrengh\": \""+String(WiFi.RSSI())+"\"";
+    }
+ 
+    if (server.arg("chipInfo")== "true"){
+        response+= ",\"chipId\": \""+String(ESP.getChipId())+"\"";
+        response+= ",\"flashChipId\": \""+String(ESP.getFlashChipId())+"\"";
+        response+= ",\"flashChipSize\": \""+String(ESP.getFlashChipSize())+"\"";
+        response+= ",\"flashChipRealSize\": \""+String(ESP.getFlashChipRealSize())+"\"";
+    }
+    if (server.arg("freeHeap")== "true"){
+        response+= ",\"freeHeap\": \""+String(ESP.getFreeHeap())+"\"";
+    }
+    response+="}";
+    */
 }
 void handle_ChangeState()
 {
@@ -189,10 +157,9 @@ void handle_ChangeState()
       RealCheck = false;
       digitalWrite(4, LOW); //выключаем
       //выключить лампочку
-    }
-    eeprom_write_state(RealCheck);
+    }    
   }
-  
+  //SetFlashState(RealCheck);
   String response = "{"; 
   String st;
   if (RealCheck == true){
@@ -203,61 +170,60 @@ void handle_ChangeState()
    }
    response+= "\"state\": \""+st+"\"";
    response+="}";
+  // SetFlashState(RealCheck);
    server.send(200, "text/html", response);
 }
 
-void handle_Reset()
-{  
-   DeviceFrendlyName = "New Robotic Device";
-   eeprom_write_state_new_device(0);   
-   eeprom_write_name(DeviceFrendlyName);
-   String response = "{"; 
-   response+= "\"name\": \""+DeviceFrendlyName+"\"";
-    response+="}";
-   server.send(200, "text/html", response);
+/*void writeEEPROM(String str) {
+   const char *buf = str.c_str();
+   EEPROM.put(AddrName, strlen(buf));
+   for (int i=0; i<16; i++) {
+       EEPROM.put(AddrName+1+i, buf[i]);
+   }
+   EEPROM.commit();
 }
 
+String readEEPROM() {
+    char buf[16];
+    uint16_t size = EEPROM.get(AddrName);
+    for (int i=0; i<size; i++) {
+      char buf[16] = EEPROM.get(AddrName+1+i);
+    }
+    return String(buf);
+}
+*/
 void handle_ChangeFrendlyName(){
+  String nm;
   if ((server.args() == 1) && (server.argName(0) == "name")) {
-    String nm;
     nm = server.arg(0);
     DeviceFrendlyName = nm;
-    eeprom_write_name(DeviceFrendlyName);
-    if (!is_new_device())
-    {
-      eeprom_write_state_new_device(1);
+    //writeEEPROM(EEPROM_NameAddr, DeviceFrendlyName);
     }
-    String response = "{"; 
-    response+= "\"name\": \""+DeviceFrendlyName+"\"";
-    response+="}";
-    server.send(200, "text/html", response);
-  }
+   String response = "{"; 
+   response+= "\"name\": \""+DeviceFrendlyName+"\"";
+   response+="}";
   
-  if ((server.args() == 1) && (server.argName(0) == "index")) {
-    int index = 0;
-    index = server.arg(0).toInt();
-    DeviceIndex = index;
-    eeprom_write_index(DeviceIndex);
-    String response = "{"; 
-    response+= "\"index\": \""+String(DeviceIndex)+"\"";
-    response+="}";
-  
-    server.send(200, "text/html", response);
-  }
-}
-
-bool is_new_device()
-{
-  if (eeprom_read_state_new_device() == 1)
-  {
-    return true;
-  }
-  else return false;
+   server.send(200, "text/html", response);
 }
 
 void loop()
 {
-  DoCheckButtonState();
+  int bt = digitalRead(5);
+  if (bt != bt_state)
+  { //если есть изменение состояния кнопки
+     if (!RealCheck)
+     {
+       RealCheck = true;
+       digitalWrite(4, HIGH); //включаем
+     }
+     else if (RealCheck)
+     {
+       RealCheck = false;
+       digitalWrite(4, LOW); //выключаем
+     }
+  }
+  bt_state = bt; //готовы снова ловить выключатель
+ // SetFlashState(RealCheck);
   server.handleClient();  
   Serial.println("DeviceFrendlyName: " + DeviceFrendlyName);
   delay(200);
