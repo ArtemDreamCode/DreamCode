22531b4eb55f355028ea09b9e033730f064842ee
Дорофеев Артем <dorofeev@kinoplan.ru>
2021-10-30 01:12:41 +0300

    new fitc scan wifi + prot shelly ap

---
+++ WiFiScaner/WiFiScaner.ino
@@ -0,0 +1,67 @@
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <FS.h>
+#include <ESP8266HTTPClient.h>
+
+struct Device
+{
+   const char* name;
+   const char* ip;
+   String ident;
+   const char* state;
+};
+
+const int DEVICES_MAX_COUNT = 20;
+Device dictionary_new[DEVICES_MAX_COUNT];
+int count_dictionary_new = 0;
+
+
+ESP8266WebServer server(80);
+
+WiFiClient client; 
+HTTPClient http; // for depricated Get request 
+
+String ShellySSID = "ShellyAP1";
+String ShellyPASS = "11001100";
+
+//String HomeSSID = "LASSARD";
+//String HomePASS = "PvE9zyZe";
+
+//String MobSSID = "Samsung";
+//String MobPASS = "11001100";
+
+String ServerSSID = "MainAP";
+String ServerPASS = "12345678";
+
+//=======================================================================
+//                     SETUP
+//=======================================================================
+void setup() {
+  Serial.begin(115200);
+  Serial.println(""); //Remove garbage
+
+  // Set WiFi to station mode and disconnect from an AP if it was previously connected
+  WiFi.mode(WIFI_STA);
+  delay(100);
+
+  Serial.println("WiFi Netwoek Scan Started");
+
+  wifi_begin(ServerSSID, ServerPASS);
+//  restServerRouting();  
+//  server.onNotFound(handleNotFound);
+//  server.begin();
+
+}
+
+//=======================================================================
+//                        LOOP
+//=======================================================================
+void loop() {
+   Serial.println("START : wifi_scan");
+ wifi_scan();
+//server.handleClient(); 
+  // Wait a bit before starting New scanning again
+  delay(5000);
+}
+//=======================================================================
---
+++ WiFiScaner/web.ino
@@ -0,0 +1,91 @@
+bool wifi_begin(String ssid, String pass){
+  bool f = false;
+  WiFi.disconnect();  //ESP has tendency to store old SSID and PASSword and tries to connect
+  WiFi.mode(WIFI_STA);
+ // WiFi.begin(ssid); 
+  WiFi.begin(ssid, pass);  
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.println("connecting to wifi");
+    delay(450);
+    f = true;
+  }
+  if (f) {
+    String ip = WiFi.localIP().toString().c_str(); 
+    Serial.println("my ip in current network: " + ip); 
+    return true;
+  }
+  return false; 
+}
+
+
+void handleNotFound() {
+  server.send(404, "text/plain", "not found");
+}
+
+void restServerRouting() 
+{  
+    server.on("/scan", handle_scan);
+}
+
+
+void handle_scan() //остановка
+{
+  /////////// сканируем точки доступа, если находим esp
+  // подключаемся к ней, отправляем данные основной точки доступа
+  // и говорим переподключиться в режиме станции
+  server.send(200, "text/html", "scan start");
+}
+
+void wifi_scan()
+{
+  Serial.println("START : wifi_scan");
+   // WiFi.scanNetworks will return the number of networks found
+  int n = WiFi.scanNetworks();
+   
+  Serial.println("Scan done");
+  
+  if (n == 0)
+    Serial.println("No Networks Found");
+  else
+  {
+    Serial.print(n);
+    Serial.println(" Networks found");
+    for (int i = 0; i < n; ++i)
+    {
+      // Print SSID and RSSI for each network found
+      Serial.print(i + 1);  //Sr. No
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i)); //SSID
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i)); //Signal Strength
+      Serial.print(") MAC:");
+      Serial.print(WiFi.BSSIDstr(i));
+      Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE)?" Unsecured":" Secured");
+      if (WiFi.SSID(i).indexOf(ShellySSID) > -1)
+      {
+        if (wifi_begin(WiFi.SSID(i), ShellyPASS))
+        {
+            String getOut = "http://192.168.4.1/reconnect?ssid="+ServerSSID+"&pass="+ServerPASS;
+            Serial.println(get(getOut));
+            wifi_begin(ServerSSID, ServerPASS);
+        }
+      }
+      
+      delay(10);
+    }
+  }
+  Serial.println("");
+}
+
+String get(String Arequest){
+    http.setTimeout(200);
+    http.begin(client, Arequest); 
+    Serial.println(Arequest);
+    int httpCode = http.GET();
+   // log(String(httpCode)); 
+     
+    String response = http.getString();  
+    Serial.println(response);   
+    http.end();
+    return response;
+}
